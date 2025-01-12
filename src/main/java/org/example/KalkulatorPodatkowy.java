c24f9716983db66a6ff21f4471691dc16c4893ce
Bartoszko <bartosz.bialoglowicz@gmail.com>
2025-01-13 00:22:21 +0100

    Initial Commit.

---
+++ src/main/java/org/example/KalkulatorPodatkowy.java
@@ -0,0 +1,59 @@
+package org.example;
+
+public class KalkulatorPodatkowy {
+
+    public enum FormaOpodatkowania {
+        RYCZALT, LINIOWY, SKALA
+    }
+
+    public static Double obliczPodatek(FormaOpodatkowania forma, double dochod, double koszty) {
+        double podstawa = dochod - koszty;
+        double podatek = 0;
+
+        switch (forma) {
+            case RYCZALT:
+                podatek = podstawa * 0.12;
+                break;
+            case LINIOWY:
+                podatek = podstawa * 0.19;
+                break;
+            case SKALA:
+                if (podstawa < 30000) {
+                    podatek = 0;
+                } else if (podstawa < 120000) {
+                    podatek = podstawa * 0.12;
+                } else {
+                    podatek = podstawa * 0.32;
+                }
+            default:
+                throw new IllegalArgumentException("Podano nieznaną formę opodatkowania");
+        }
+        return podatek;
+    }
+
+    public static double obliczSkladkeZdrowotna(FormaOpodatkowania forma, double dochod, double koszty) {
+        double podstawa = dochod - koszty;
+        double skladka = 0;
+
+        switch (forma) {
+            case LINIOWY:
+                skladka = podstawa * 0.09;
+                break;
+            case SKALA:
+                skladka = podstawa * 0.09;
+                break;
+            case RYCZALT:
+                if (podstawa < 60000) {
+                    skladka = 12 * 376;
+                } else if (podstawa < 3000000) {
+                    skladka = 12 * 626;
+                } else {
+                    skladka = 12 * 1128;
+                }
+            default:
+                throw new IllegalArgumentException("Podano nieznaną formę opodatkowania");
+        }
+
+        return skladka;
+    }
+}
